0b853e2b7b554e69781b908128969d53d6055ade
gashumba aimable pacifique <39914832+GASHUMBA@users.noreply.github.com>
2024-12-31 15:32:55 +0200

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,1 +1,9 @@
+<?php
+$con = mysqli_connect("bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","rr0t2iygn1fk8h6r","h3llzm4gk3kb0uks","t4exgzzf9sqba2p5");
 
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?><!-- Visit codeastro.com for more projects -->
