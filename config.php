2ed4f3cd349a3bcd41464272952e4b2a161ab616
gashumba aimable pacifique <39914832+GASHUMBA@users.noreply.github.com>
2024-12-31 15:26:08 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,1 @@
-<?php
-// Database connection details from Heroku Environment Variable
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-// Create connection
-$conn = new mysqli($server, $username, $password, $db);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully!";
-?>
